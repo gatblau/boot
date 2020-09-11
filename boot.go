6cb0f75bf98e7e3a75eef597263bc8de485d956c
gatblau <onix@gatblau.org>
2020-09-11 08:46:53 +0100

    fail if variables are defined but not found

--- boot.go
+++ boot.go
@@ -33,51 +33,61 @@ func main() {
 		log.Fatal().Msgf("you must provide files to merge!")
 	}
 
+	// loop through the specified configuration files
 	for _, file := range files {
 		merged := false
+		// read the file content
 		bytes, err := ioutil.ReadFile(file)
 		if err != nil {
 			log.Fatal().Msgf("cannot read file %s: %s", file, err)
 		}
 		content := string(bytes)
-
+		// find all environment variable placeholders in the content
 		vars := regex.FindAll(bytes, 1000)
+		// loop though the found vars to merge
 		for _, v := range vars {
 			defValue := ""
+			// removes placeholder marks: ${...}
 			vname := strings.TrimSuffix(strings.TrimPrefix(string(v), "${"), "}")
 			// is a default value defined?
 			cut := strings.Index(vname, ":")
+			// split default value and var name
 			if cut > 0 {
 				// get the default value
 				defValue = vname[cut+1:]
+				// get the name of the var without the default value
 				vname = vname[0:cut]
 			}
-			// retrieve the env variable
+			// check the name of the env variable is not "PWD" as it can return the current directory in some OSs
+			if vname == "PWD" {
+				log.Fatal().Msgf("environment variable cannot be PWD, choose a different name")
+			}
+			// fetch the env variable value
 			ev := os.Getenv(vname)
-			// if the env variable is not defined
+			// if the variable is not defined in the environment
 			if len(ev) == 0 {
 				// if no default value has been defined
 				if len(defValue) == 0 {
-					log.Error().Msgf("environment variable '%s' not defined, skipping merging", vname)
+					log.Fatal().Msgf("environment variable '%s' and/or default value not defined, skipping merging", vname)
 				} else {
-					// merge the default value
+					// merge with the default value
 					content = strings.Replace(content, string(v), defValue, 1000)
 					merged = true
 					log.Info().Msgf("merged placeholder %s with default value '%s'", string(v), defValue)
 				}
 			} else {
-				// merge the env variable value
+				// merge with the env variable value
 				content = strings.Replace(content, string(v), ev, 1000)
 				merged = true
 				log.Info().Msgf("merged placeholder %s with value '%s'", string(v), ev)
 			}
 		}
-		// if variables have been merged
+		// if variables have been merged at all
 		if merged {
 			// override file with merged values
 			err = WriteToFile(file, content)
 			if err != nil {
-				log.Error().Msgf("cannot update config file: %s", err)
+				log.Fatal().Msgf("cannot update config file: %s", err)
 			}
 		}
 	}
@@ -90,11 +100,10 @@ func WriteToFile(filename string, data string) error {
 		return err
 	}
 	defer file.Close()
-
 	_, err = io.WriteString(file, data)
 	if err != nil {
 		return err
 	}
-	log.Info().Msgf("'%v' characters written to file '%s'", len(data), filename)
+	log.Info().Msgf("'%v' bytes written to file '%s'", len(data), filename)
 	return file.Sync()
 }
--- readme.md
+++ readme.md
@@ -1,30 +1,73 @@
 <a href="https://icon-icons.com/icon/boot-stomp/39342"><img src="boot.png" alt="By Lorc, Delapouite & contributors" width="100" height="100" align="right" /></a>
-# Boot 
 
-If an application uses static configuration files, boot can read environment variables and merge them into one or more configuration files.
+# BOOT
 
-# Use
+*BOOT* is a utility that merges environment variables into (configuration) files. It is designed to pass environment variables to applications with static configuration files running inside Docker containers.
+
+It is an alternative to overlaying the configuration file on the container on start up.
+
+Simply:
+
+- add environment variable placeholders into the files
+- export environment variables with the required values
+- call *boot* passing the file names to be updated
+- launch your app
+
+## Getting Started
+
+1. Download the *boot* utility platform specific versions from [here](./build).
+
+2. Create a bootstrap file to merge the variables and launch the app like follows:
+
+```shell script
+# start.sh
+/app/boot /app/conf1 /app/conf2 ...
+exec "$@"
+```
+
+3. Copy boot and the configuration with placehoslders to the docker image
+
+4. Call the bootstrap script from the CMD line (mosquitto mqtt broker example):
+   
+```dockerfile   
+CMD ["sh", "/app/start.sh", "/app/mosquitto", "-c", "/app/mosquitto.conf"]
+```
+
+## Merging Vars
 
 given the following *test.cfg* config file:
+
 ```shell script
 cfg1=${ENV_VAR_1}
+# ADS2 is a default value
 cfg2=${ENV_VAR_2:ASD2}
 cfg3=${ENV_VAR_3}
 cfg4=${ENV_VAR_4}
 ```
 
+on the command line run:
+
 ```shell script
 # export environment variables
+# note no variable ENV_VAR_2 is defined 
 export ENV_VAR_1=AAA
-export ENV_VAR_2=BBB
 export ENV_VAR_3=CCC
-export ENV_VAR_3=DDD
+export ENV_VAR_4=DDD
 
 # merge variables in the config file
 ./boot test.cfg
 ```
 
-# Download binaries
+check the contents of the file:
 
-Platform specific versions can be found [here](./build).
+```shell script
+cfg1=AAA
+# merged default
+cfg2=ASD2
+cfg3=CCC
+cfg4=DDD
+```
 
+**NOTE**: using "PWD" as environemtn variable is not allowed as it can retrieve the path of the current process in some OS.
+
+If no variables are exported the execution will fail.
